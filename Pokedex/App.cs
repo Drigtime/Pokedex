d86ddb3f230a0caf53b4326607643184c9387b6c
FlxLtg <fleturgez@gmail.com>
2020-10-19 17:06:34 +0200

    add interface

---
+++ Pokedex/App.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Terminal.Gui;
+
+namespace Pokedex
+{
+    class App : Window
+    {
+        static App()
+        {
+            Application.Init();
+
+            var top = new Toplevel()
+            {
+                X = 0,
+                Y = 0,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+
+            var win = new Window("Pokedex")
+            {
+                X = 0,
+                Y = 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+
+            var menu = new MenuBar(
+                new MenuBarItem[] {
+                    //new MenuBarItem ("_File", new MenuItem [] {
+                        new MenuBarItem ("_Quit", "", () => {
+                            Application.Shutdown ();
+                            Environment.Exit(0);
+                        //})
+                    }),
+                });
+
+            var list = new Window("List")
+            {
+                X = 0,
+                Y = 6,
+                Width = Dim.Percent(50),
+                Height = Dim.Fill()
+            };
+
+            var specs = new Window("Specs")
+            {
+                X = Pos.Percent(50),
+                Y = 6,
+                Width = Dim.Percent(50),
+                Height = Dim.Fill()
+            };
+
+            var researchLabel = new Label("Recherche :")
+            {
+                X = 2,
+                Y = 3,
+                Width = 30,
+                Height = 1
+            };
+
+            var research = new TextField("")
+            {
+                X = 2,
+                Y = 4,
+                Width = 30,
+                Height = 1
+            };
+
+            var previousButton = new Button("Previous");
+            previousButton.X = 0;
+            previousButton.Y = Pos.AnchorEnd(1);
+
+            var nextButton = new Button("Next");
+            nextButton.X = Pos.AnchorEnd() - (Pos.Right(nextButton) - Pos.Left(nextButton));
+            nextButton.Y = Pos.AnchorEnd(1);
+
+            list.Add(previousButton, nextButton);
+            top.Add(menu, win, list, specs, research, researchLabel);
+            Application.Run(top);
+
+        }
+    }
+}
--- Pokedex/Program.cs
+++ Pokedex/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Pokedex.PokeApi;
 using System;
 using System.Threading.Tasks;
+using Terminal.Gui;
 
 namespace Pokedex
 {
@@ -8,10 +9,14 @@ namespace Pokedex
     {
         static void Main(string[] args)
         {
-            RunAsync().GetAwaiter().GetResult();
+            //interface de l'app
+            Application.Run<App>();
+
+            List().GetAwaiter().GetResult();
+            ListAll().GetAwaiter().GetResult();
         }
 
-        static async Task RunAsync()
+        static async Task List()
         {
             ApiHelper apiHelper = new ApiHelper(new Uri("https://pokeapi.co/api/v2/"));
             NamedAPIResourceList resourceList = await apiHelper.CallWebAPIAsync("pokemon?limit=100&offset=200");
@@ -21,6 +26,21 @@ namespace Pokedex
                     Count: {resourceList.Count}
                     Next: {resourceList.Next}
                     Previous: {resourceList.Previous}
+                    Result: [
+                        name: {resourceList.Results[0].Name}
+                        url: {resourceList.Results[0].Url}
+                    ]"
+                );
+        }
+
+        static async Task ListAll()
+        {
+            ApiHelper apiHelper = new ApiHelper(new Uri("https://pokeapi.co/api/v2/"));
+            NamedAPIResourceList resourceList = await apiHelper.CallWebAPIAsync("pokemon");
+
+            Console.WriteLine(
+                    @$"
+                    Count: {resourceList.Count}
                     Result: [
                         name: {resourceList.Results[0].Name}
                         url: {resourceList.Results[0].Url}
