f07b6b90055dd7f3d028c0271e0d63f2eea5f350
drigtime <drigtime@WIN-KSKC18LIGBD>
2021-01-04 12:06:39 +0100

    Missing file

---
+++ ApiHelper/ApiHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ApiHelper
+{
+    public class ApiHelper<T>
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiHelper(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<T> GenericCallWebApiAsync(Uri uri)
+        {
+            //using var client = new HttpClient();
+            T resourceList = default;
+
+            // client.DefaultRequestHeaders.CacheControl.Public = true;
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //GET Method  
+            var response = await _httpClient.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                resourceList = JsonConvert.DeserializeObject<T>(data);
+            }
+
+            return resourceList;
+        }
+    }
+}
---
+++ PokedexXUnitTests/Pokedex.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace PokedexXUnitTests
+{
+    public class Pokedex
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
